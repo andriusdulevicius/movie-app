761b36128ff4366d44cbf002e59f9fac3537b494
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-09 11:33:52 +0300

    movie table comonent created , added to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import MovieTable from './components/movieTable';
 
 function App() {
   return (
     <div className='App'>
       <h2>Movie App</h2>
+      <MovieTable />
     </div>
   );
 }
---
+++ src/components/movieTable.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import { getMovies, getMovie } from './services/fakeMovieService';
+
+class MovieTable extends Component {
+  state = {};
+
+  render() {
+    return <h3>Movie table is here</h3>;
+  }
+}
+
+export default MovieTable;
