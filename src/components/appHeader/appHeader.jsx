dd04f745091388453c9448ff45755c504f7928df
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-11 10:03:56 +0300

    hero image

--- src/components/appHeader/appHeader.jsx
+++ src/components/appHeader/appHeader.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import goldImg from './gold.jpg';
+import './style.css';
 
 class AppHeader extends Component {
   render() {
-    return <h2>I am header</h2>;
+    return (
+      <header>
+        <img className='hero' src={goldImg} alt='very nice background leaves' />
+        <a></a>
+      </header>
+    );
   }
 }
 
