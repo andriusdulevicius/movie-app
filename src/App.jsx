165b1958a9d617adec0e3d38ca46a66204db92f7
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-11 09:52:55 +0300

    css added and works

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import AppHeader from './components/appHeader/appHeader';
 import AppList from './components/appList/appList';
 import AppAddTodo from './components/appAddTodo/appAddTodo';
+//app styles
+import './app.css';
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div className='App'>
+      <div className='app'>
         <h1>Simple Todo App</h1>
         <AppHeader />
         <AppList />
