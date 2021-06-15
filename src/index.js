37c19e056951828d180a07bf13d7e0f33dd6bc3a
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-15 10:24:45 +0300

    install router , set up index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 // import FetchTest from './components/fetchTest';
 import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
